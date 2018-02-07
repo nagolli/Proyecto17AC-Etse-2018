e9c554a7af53cde60bbd1ce77d3b3a4b5f8c307a
nagolli <nacho.gomis@gmail.com>
2018-02-07 19:21:49 +0100

    Integracion de funciones 3 y 4 en main y Test de unidad

--- Test de unidad.c
+++ Test de unidad.c
@@ -27,3 +27,71 @@ int main()
     
     return resultado /*== resultadoNumerico estandar*/;
 }
+
+
+/**
+ * CompletarMatriz funcion que calcula el algoritmo Needleman-Wunsch para una matriz
+ * @author Nacho
+ * @date 7/2/2018
+ * @param string1 Cadena de texto 1
+ * @param string2 Cadena de texto 2
+ * @param matrix Matriz de Celdas, su tamaño debe ser el de las cadenas de texto +1
+ */
+void CompletarMatriz(char* string1,char* string2,struct Celda** matrix)
+{
+    unsigned i;
+    unsigned j;
+    unsigned size1=strlen(string1);
+    unsigned size2=strlen(string2);
+    for(i=1;i<=size1;i++)
+        for(j=1;j<=size2;j++)
+            CalcularCasilla(i, j, (string1[i-1]==string2[j-1]), matrix);
+    return;
+}
+
+/**
+ * CompletarFila funcion que calcula el algoritmo Needleman-Wunsch para una fila, preparado para paralelizar una matriz, que en ese caso se pasa i=1
+ * @author Nacho
+ * @date 7/2/2018
+ * @param string1 Cadena de texto 1
+ * @param string2 Cadena de texto 2
+ * @param matrix Matriz de Celdas, su tamaño debe ser el de las cadenas de texto +1
+ * @param i indice de fila a rellenar
+ */
+void CompletarFila(char* string1,char* string2,struct Celda** matrix, unsigned i)//Para empezar de 0 dar i=1
+{
+    unsigned j;
+    unsigned size1=strlen(string2);
+    unsigned size2=strlen(string2);
+        CalcularCasilla(i, 1, (string1[i-1]==string2[0]), matrix);
+        if(i<=size1)
+        {
+            //Lanzar siguiente fila
+        }
+        for(j=2;j<=size2;j++)
+            CalcularCasilla(i, j, (string1[i-1]==string2[j-1]), matrix);
+    return;
+}
+
+/**
+ * CalcularCasilla funcion que calcula el contenido de una casillo de la matriz para el algoritmo Needleman-Wunsch
+ * @author Nacho
+ * @date 6/2/2018
+ * @param i Indice de fila (No puede ser 0)
+ * @param j Indice de columna (No puede ser 0)
+ * @param igual Comparativa (Char==Char)
+ * @param matrix Matriz de structs sobre la que se opera. In/Out
+ */
+void CalcularCasilla(unsigned i, unsigned j, bool igual, struct Celda **matrix)
+{
+    // Constantes Match 2, -2 dismatch, -1 Hueco
+    int A = matrix[i-1][j].score - 1;
+    int B = matrix[i][j-1].score - 1;
+    int C = matrix[i-1][j-1].score+ (igual*4)-2; //C= arg + (argB*(Match-Fallo))+Fallo
+    
+    matrix[i][j].lateral = (A>=B && A>=C);
+    matrix[i][j].arriba  = (B>=A && B>=C);
+    matrix[i][j].diag    = (C>=B && C>=A);
+    
+    matrix[i][j].score=((A* matrix[i][j].lateral)+(B* matrix[i][j].arriba)+(C* matrix[i][j].diag)) / (matrix[i][j].arriba+matrix[i][j].lateral+matrix[i][j].diag);
+}
